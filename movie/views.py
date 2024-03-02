629760b4706341c1075482d0ab6f02f2e5ae1f17
JPZU <juanpablozp@icloud.com>
2024-03-02 15:45:29 -0500

    Complete graphic per genre

--- movie/views.py
+++ movie/views.py
@@ -32,24 +32,27 @@ def statistics_view(request):
     matplotlib.use('Agg')
     all_movies = Movie.objects.all()
     
-    movie_counts_by_year = {}
+    movie_counts_by_genre = {}
     
     
     for movie in all_movies:
-        year = movie.year if movie.year else "None"
-        if year in movie_counts_by_year:
-            movie_counts_by_year[year] +=1
+        
+        genres = movie.genre.split(',') 
+        genre = genres[0].strip() if genres else "None"
+        
+        if genre in movie_counts_by_genre:
+            movie_counts_by_genre[genre] += 1
         else:
-            movie_counts_by_year[year] =1
+            movie_counts_by_genre[genre] = 1
             
     bar_width = 0.5
-    bar_positions = range(len(movie_counts_by_year))
+    bar_positions = range(len(movie_counts_by_genre))
     
-    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
-    plt.title('Movies per year')
-    plt.xlabel('Year')
+    plt.bar(bar_positions, movie_counts_by_genre.values(), width=bar_width, align='center')
+    plt.title('Movies per genre')
+    plt.xlabel('Genre')
     plt.ylabel('Number of movies')
-    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+    plt.xticks(bar_positions, movie_counts_by_genre.keys(), rotation=90)
     plt.subplots_adjust(bottom=0.3) 
     buffer = io.BytesIO()
     plt.savefig(buffer, format='png')
